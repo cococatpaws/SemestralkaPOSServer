fdc326855f1ce52e4cc8de149159894499fb6dc1
Veronika Rybanská <nikarybanska01@gmail.com>
2024-01-07 17:12:45 +0100

    skoro hotovy server-upravene4

--- main.c
+++ main.c
@@ -23,21 +23,30 @@ typedef struct thread_data {
     struct buffer_vzor buf;
     short port;
     ACTIVE_SOCKET* my_socket;
+    pthread_mutex_t zapis_mutex;
+    PASSIVE_SOCKET* passiveSocket;
+    int pocetPripojenych;
 } THREAD_DATA;
 
 void zapis(char * string);
 
 void thread_data_init(struct thread_data* data, int buffer_capacity,
-                      short port, ACTIVE_SOCKET* my_socket) {
+                      short port, ACTIVE_SOCKET* my_socket,PASSIVE_SOCKET* passiveSocket) {
     buffer_vzor_init(&data->buf, buffer_capacity);
     data->port = port;
     data->my_socket = my_socket;
+    data->passiveSocket = passiveSocket;
+    pthread_mutex_init(&data->zapis_mutex,NULL);
+    data->pocetPripojenych =0;
 }
 
 void thread_data_destroy(struct thread_data* data) {
     buffer_vzor_destroy(&data->buf);
     data->port = 0;
     data->my_socket = NULL;
+    data->passiveSocket = NULL;
+    pthread_mutex_destroy(&data->zapis_mutex);
+    data->pocetPripojenych =0;
 }
 
 void* consume(void* thread_data);
@@ -50,34 +59,16 @@ void* read_active_socket(void* thread_data) {
 
 void* process_client_data(void* thread_data) {
     THREAD_DATA *data = (THREAD_DATA *)thread_data;
-    PASSIVE_SOCKET p_socket;
-    passive_socket_init(&p_socket);
-    passive_socket_start_listening(&p_socket, data->port);
-    while(passive_socket_is_listening(&p_socket)) {
-        passive_socket_wait_for_client(&p_socket, data->my_socket);
+    passive_socket_start_listening(data->passiveSocket, data->port);
+    while(passive_socket_wait_for_client(data->passiveSocket, data->my_socket)) {
         pthread_t  client;
+        data->pocetPripojenych++;
         pthread_create(&client, NULL,read_active_socket,data);
         pthread_join(client, NULL);
     }
-    passive_socket_stop_listening(&p_socket);
-    passive_socket_destroy(&p_socket);
     return NULL;
 }
 
-void try_get_client_vzor(struct active_socket* my_sock, struct vzor* vzor) {
-    CHAR_BUFFER r_buf;
-    char_buffer_init(&r_buf);
-
-    if(active_socket_try_get_read_data(my_sock, &r_buf)) {
-        if(r_buf.size > 0) {
-            if(active_socket_is_end_message(my_sock, &r_buf)) {
-            } else {
-                vzor->vzor  = r_buf.data;
-            }
-        }
-    }
-    char_buffer_destroy(&r_buf);
-}
 
 int pocetRiadkovVSubore() {
     FILE * p_soubor = fopen("cmake-build-debug-frios2/zoznam_vzorov.txt", "r");
@@ -173,8 +164,16 @@ void* consume(void* thread_data) {
             else if (isNumeric(string)) {
                 int cisloRiadku = atoi(string);
                 citaj(data, cisloRiadku);
-            } else {
+            } else if (strcmp(string,data->my_socket->end_message) == 0) {
+                data->pocetPripojenych--;
+                if(data->pocetPripojenych == 0) {
+                    passive_socket_stop_listening(data->passiveSocket);
+                }
+            }
+            else {
+                pthread_mutex_lock(&data->zapis_mutex);
                 zapis(string);
+                pthread_mutex_unlock(&data->zapis_mutex);
             }
 
     }
@@ -197,12 +196,15 @@ int main() {
     pthread_t th_receive;
     struct thread_data data;
     struct active_socket my_socket;
+    PASSIVE_SOCKET p_socket;
+    passive_socket_init(&p_socket);
     active_socket_init(&my_socket);
-    thread_data_init(&data, 10, 12389, &my_socket);
+    thread_data_init(&data, 10, 12389, &my_socket,&p_socket);
     pthread_create(&th_receive, NULL, process_client_data, &data);
     pthread_join(th_receive, NULL);
 
     thread_data_destroy(&data);
     active_socket_destroy(&my_socket);
+    passive_socket_destroy(&p_socket);
     return 0;
 }
